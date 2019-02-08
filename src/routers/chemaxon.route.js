d1447ba0b1c5f70dfe6829a3403296cce805d0bb
dr. Lantos Tibor <lantost@intranet.local>
2019-02-08 12:37:28 +0100

    FEAT: add comparison fn to post endpoint

--- src/routers/chemaxon.route.js
+++ src/routers/chemaxon.route.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import getImageData from '../api/getImageData';
-// import getStructureData from '../api/getStructureData';
+import getStructureData from '../api/getStructureData';
 
 const chemAxon = express.Router();
 
@@ -9,21 +9,39 @@ chemAxon
 
   .post(async (req, res, next) => {
     console.log('req: ', req.body);
-    const name = req.body.name;
-    const imageData = await getImageData({
-      'structure': name,
-      'format': 'SVG',
-    });
+    const names = req.body.names;
+    const result = {
+      'compounds': [],
+      'comparison': null,
+    };
 
-    // const structureData = await getStructureData({
-    //   'searchType': 'SUBSTRUCTURE',
-    //   'query': 'aspirin',
-    //   'target': 'aspirin',
-    // });
-
+    // get images
+    for (let i = 0; i < names.length; i++) {
+      const compound = names[i];
+      const imageData = await getImageData({
+        'structure': compound,
+        'format': 'SVG',
+      });
+      result.compounds.push({
+        'compoundName': compound,
+        'rawImg': imageData.image,
+      });
+      
+    }
+  
+    // get comparison data
+    if (names.length === 2) {
+      const structureData = await getStructureData({
+        'searchType': 'SUBSTRUCTURE',
+        'query': names[0],
+        'target': names[1],
+      });
+      console.log('structureData: ', structureData);
+      result.comparison = structureData.matching;
+    }
+    
     return res.json(
-      imageData,
-      // structureData,
+      result
     );
   })
 
